eb6036a488415ebc814be3ef5bc470ab805cf86a
Adam Brown <adam.brown.betty@gmail.com>
2019-08-22 02:24:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ def long_planeteer_calls(array_of_calls)
   end
 end
 
-def find_the_cheese(food)
+def find_the_cheese(foods)
     cheese_types = ["cheddar", "gouda", "camembert"]
-  food.include?(cheese_types)
+  foods.include?(cheese_types)
 end
