be7956b080cb42a64ce6c8dfdefd252d1d688776
Adam Brown <adam.brown.betty@gmail.com>
2019-08-22 01:00:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,10 @@ def roll_call_dwarves(dwarves)
   
    [dwarves].each_with_index  { |item, index|
   puts dwarves[item] = index
+  
+  .each.with_index(1) do |elem, i|
+    puts "#{i}: #{elem}"
+end
 }
 end
 
