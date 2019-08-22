85946beefc6014819215c18f9778a1765ebf4cd1
Adam Brown <adam.brown.betty@gmail.com>
2019-08-22 01:44:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,13 +6,13 @@ def roll_call_dwarves(dwarves)
 end
 
 
-
 def summon_captain_planet(planeteer_calls)
  new_calls = planeteer_calls.map do |name| name.capitalize 
  end 
  new_calls.collect do |x| x + "!"      
   end
 end
+
 
 def long_planeteer_calls(array_of_calls)
   
